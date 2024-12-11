6a6ea3356d589970a593024e92b65c93c1ec02f8
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2024-12-11 08:25:08 +0530

    Relation 3
    Relation 3

---
+++ Relation/Prob3/abhay/Relation3/Person.java
@@ -0,0 +1,21 @@
+package Prob3.abhay.Relation3;
+
+public class Person {
+	private String name;
+	private Address address;
+	public Person(String name, Address address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", address=" + address + "]";
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Relation/Prob3/abhay/Relation3/Test.java
@@ -0,0 +1,10 @@
+package Prob3.abhay.Relation3;
+
+public class Test {
+  public static void main(String[] args) {
+	Address a1=new Address("ammerpet","HYDERABAD");
+	Person p1 =new Person("abhay", a1);
+	System.out.println(p1);
+}
+
+}
