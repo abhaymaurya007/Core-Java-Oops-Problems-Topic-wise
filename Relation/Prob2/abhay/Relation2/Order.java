e001a7c3a796e23f7b09f91d09f66d30c577b4d7
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2024-12-11 08:23:09 +0530

    Relation 2
    Relation 2

---
+++ Relation/Prob2/abhay/Relation2/Order.java
@@ -0,0 +1,28 @@
+package Prob2.abhay.Relation2;
+
+public class Order {
+	private String orderId;
+	private String productName;
+	public Order(String orderId, String productName) {
+		super();
+		this.orderId = orderId;
+		this.productName = productName;
+	}
+//	public String getOrderId() {
+//		return orderId;
+//	}
+//	public void setOrderId(String orderId) {
+//		this.orderId = orderId;
+//	}
+	public String getProductName() {
+		return productName;
+	}
+//	public void setProductName(String productName) {
+//		this.productName = productName;
+//	}
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", productName=" + productName + "]";
+	}
+	
+}
