1fc2e4de9dc6e6c8a2d20d6ffa6d672c4ea6e986
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2024-12-12 21:37:20 +0530

    update
    update

--- Interface/Prob3/abhay/interface3/MobilePhoneInterface.java
+++
@@ -1,8 +0,0 @@
-package Prob3.abhay.interface3;
-
-public interface MobilePhoneInterface {
-    void insertSim(SIMCard s1);
-    void Remove();
-    void makeCall(String l);
-    void makeMassage(String m1);
-}
